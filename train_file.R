8d09157f6c8084acfa7218aad0e3f161d515c1c9
npham22 <npham22@cougarnet.uh.edu>
2022-04-25 03:18:29 -0500

    Updated code

--- train_file.R
+++ train_file.R
@@ -1,4 +1,5 @@
-## train file
+## train file for neural network
+options(java.parameters = c("-XX:+UseConcMarkSweepGC", "-Xmx8192m"))
 df = read.csv('F:/OneDrive - University Of Houston/Study/11th semester/MATH 4322/GP/creditcard.csv')
 df$Time = NULL
 df$Amount = scale(df$Amount)
@@ -11,12 +12,12 @@ library(doSNOW) # for parallel computing
 library(PRROC) # for evaluation metrics
 
 set.seed(1)
-cvIndex = createFolds(df$Class, k = 5, returnTrain = T)
-trControl = trainControl(index = cvIndex, method = "cv", classProbs = TRUE, summaryFunction = prSummary, allowParallel = TRUE, verboseIter = TRUE, savePredictions = TRUE, search = "random")
-cl = makeCluster(7, outfile="")
+cvIndex = createFolds(df$Class, k = 10, returnTrain = T)
+cl = makeCluster(8, outfile="")
 registerDoSNOW(cl)
 
+trControl = trainControl(index = cvIndex, method = "cv", returnData = FALSE, classProbs = TRUE, summaryFunction = prSummary, allowParallel = TRUE, verboseIter = TRUE, savePredictions = TRUE)
 set.seed(1)
-NN = train(Class ~ ., data = df, method = "nnet", tuneLength = 50, metric = "AUC", trControl = trControl, verbose = TRUE)
+NN_last2 = train(Class ~ ., data = df, method = "nnet", maxit = 200, metric = "AUC", trControl = trControl, verbose = TRUE, tuneGrid = expand.grid(size = sample(15:30, size = 400, replace = TRUE), decay = 10^runif(400, min = -5, 1)))
 stopCluster(cl)
-NN
+save(NN_last2, file = "NN_last2.Rdata")
